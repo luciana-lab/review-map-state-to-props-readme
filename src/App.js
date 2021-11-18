cec85f2631fc3c81401e768c22d6cea8640b9303
Luciana Sugeta <lulusugeta@gmail.com>
2021-11-18 09:53:42 -0500

    commit tree 704f4c8d35ebf6a0ae6919278cd216a8a0726e41

--- src/App.js
+++ src/App.js
@@ -21,21 +21,33 @@ class App extends Component {
     // debugger;
     return (
       <div className="App">
-          <button onClick={() => this.handleOnClickItems()}>
-            Click to change items count
-            </button>
-          <button onClick={() => this.handleOnClickUsers()}>
-            Click to change user count
-          </button>
-          <p>{this.props.items.length}</p>
+        <button onClick={() => this.handleOnClickItems()}>
+          Click to change items count
+        </button>
+        <button onClick={() => this.handleOnClickUsers()}>
+          Click to change user count
+        </button>
+        <p>{this.props.items.length}</p>
       </div>
     );
   }
 }
 
+// the mapStateToProps() method is executed with each change to the store's state.
+// mapStateToProps() receives state as its first argument
+// mapSrareToProps() can return just a portion of the state or ignore the store's state and return whatever it likes
 const mapStateToProps = (state) => {
-  debugger;
-  return { items: state.items }
+  // debugger;
+  return { itens: state.items }
 }
 
+// export default connect(state => ({ items: state.items }))(App) -> shorten version of mapStateToProps()
 export default connect(mapStateToProps)(App);
+// connect is connecting the App to a slice of the store's state specified in mapStateToProps()
+// connect() calls the mapStateToProps() function each time there is a change in state
+// connect() takes whatever the return value is of the mapStateToProps() and passes it to the props of the component in the last parentheses (in this case, App)
+
+
+// mapStateToProops() is called each time there is a change of state, and has access to the store's entire state.
+// The connect() function then takes the return value from the mapStateToProps() function and adds that return value to the props of the component that is passed through in the last set of parentheses. 
+// We call that component a connected component because it is connected to the store.
